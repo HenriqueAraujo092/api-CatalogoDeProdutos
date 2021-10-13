4d57f13a4abca96c9a46c576a827437dd268c4a3
HenriqueAraujo092 <henrique.araujo092@hotmail.com>
2021-10-13 14:39:18 -0300

    Primeiro commit

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [key]
+        public int ProdutoId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+        [MaxLength(500)]
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
---
+++ APICatalogo/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace APICatalogo.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
